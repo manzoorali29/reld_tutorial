3ea2610aa48260232ea7a6d7a84db49f1f887d71
Manzoor Ali <manzoorali29@gmail.com>
2022-12-25 10:36:00 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'RELD'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, DICE Group'
+author = 'Manzoor Ali'
 
 release = '0.1'
 version = '0.1.0'
